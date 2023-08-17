2b2b968678acd5ae7823242acbea7e1eefe40348
Ahmed Abdelaziz <ahmed0abdelaziz@gmail.com>
2023-08-17 16:51:26 +0300

    adding cart bar to product details

---
+++ src/app/products/components/cart-bar/cart-bar.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-bar',
+  templateUrl: './cart-bar.component.html',
+  styleUrls: ['./cart-bar.component.scss']
+})
+export class CartBarComponent implements OnInit{
+cartProducts:any[] = [];
+@Input() productDetails:any;
+@Output() item = new EventEmitter();
+amount:number = 1;
+buttonDisabled:boolean = false;
+ngOnInit(): void {
+    this.checkIfProductInCart()
+}
+addProductToCart(button:any,word:string){
+  button.textContent = word
+  button.disabled = true;
+  this.item.emit({item:this.productDetails,quantity:this.amount});
+}
+
+checkIfProductInCart(){
+  if('cart' in localStorage) {
+    this.cartProducts = JSON.parse(localStorage.getItem('cart')!);
+    console.log(this.cartProducts);
+    console.log(this.productDetails.id);
+    let existed = this.cartProducts.find(item=>item.item.id === this.productDetails.id);
+    if(existed){
+       this.buttonDisabled = true;
+    }
+
+  }
+}
+
+}
