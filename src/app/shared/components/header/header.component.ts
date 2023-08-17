333c6424fc24e0b4c28d6e8fc289002851e3e95d
Ahmed Abdelaziz <ahmed0abdelaziz@gmail.com>
2023-08-17 16:52:01 +0300

    [header] add count of cart length

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  cartProducts:any[] = [];
+  cartLength:number = 0;
+@HostBinding('class.nav-opened') navbarOpened = false;
+  navMobileToggle(){
+    console.log('clicked');
+    this.navbarOpened = !this.navbarOpened
+    console.log(this.navbarOpened);
+  }
+  ngOnInit(): void {
+    this.getCartLength()
+  }
+  getCartLength(){
+    if('cart' in localStorage) {
+      this.cartProducts = JSON.parse(localStorage.getItem('cart')!);
+      this.cartLength = this.cartProducts.length;
+    }
+  }
 
 }
